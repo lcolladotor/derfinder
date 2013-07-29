a95aeb85095ea49262a2a052c3bfc278463b10ad
lcolladotor <lcollado@jhsph.edu>
2013-07-29 11:55:09 -0400

    Modularized loadCoverage() by creating makeBamList() from it's code

--- R/loadCoverage.R
+++ R/loadCoverage.R
@@ -2,14 +2,11 @@
 #'
 #' For a group of samples this function reads the coverage information for a specific chromosome directly from the BAM files. It then merges them into a DataFrame and removes the bases that do not pass the cutoff.
 #' 
+#' @param dirs A character vector with the full path to the sample BAM files. The names are used for the column names of the DataFrame. Check \link{makeBamList} for constructing \code{dirs}.
 #' @param chr Chromosome to read. Should be in simple format. For example, use X and not chrX.
-#' @param datadir The main directory where each of the \code{sampledirs} is a sub-directory of \code{datadir}.
-#' @param sampledirs A character vector with the names of the sample directories. If \code{datadir} is \code{NULL} it is then assumed that \code{sampledirs} specifies the full path to each sample.
-#' @param samplepatt If specified and \code{sampledirs} is set to \code{NULL}, then the directories matching this pattern in \code{datadir} (set to \code{.} if it's set to \code{NULL}) are used as the sample directories.
 #' @param cutoff Per base pair, at least one sample has to have coverage greater than \code{cutoff} to be included in the result.
 #' @param chrlen The chromosome length in base pairs. If it's \code{NULL}, the chromosome length is extracted from the BAM files.
-#' @param bamterm Name of the BAM file used in each sample. By default it is set to \code{accepted_hits.bam} since that is the automatic name generated when aligning with TopHat. If \code{NULL} it is then ignored when reading the BAM files. This can be useful if all the BAM files are stored in a single directory.
-#' @param output If \code{NULL} then no output is saved in disk. If \code{auto} then an automatic name is constructed (chrXDF.Rdata for example). If another character is specified, then that name is used for the output file.
+#' @param output If \code{NULL} then no output is saved in disk. If \code{auto} then an automatic name is constructed (chrXCovInfo.Rdata for example). If another character is specified, then that name is used for the output file.
 #' @param verbose If \code{TRUE} basic status updates will be printed along the way.
 #'
 #' @return A list with two components.
@@ -26,11 +23,12 @@
 #' @importMethodsFrom Rsamtools names
 #' @examples
 #' datadir <- system.file("extdata", "genomeData", package="derfinder2")
-#' ## Reading the data and filtering it is quite fast.
-#' system.time(data <- loadCoverage(chr="21", datadir=datadir, samplepatt="*accepted_hits.bam$", cutoff=0, bamterm=NULL))
-#'
+#' dirs <- makeBamList(datadir=datadir, samplepatt="*accepted_hits.bam$", bamterm=NULL)
 #' ## Shorten the column names
-#' colnames(data$coverage) <- gsub("_accepted_hits.bam", "", colnames(data$coverage))
+#' names(dirs) <- gsub("_accepted_hits.bam", "", names(dirs))
+#' 
+#' ## Reading the data and filtering it is quite fast.
+#' system.time(data <- loadCoverage(dirs=dirs, chr="21", cutoff=0))
 #' data
 #'
 #' ## For other examples this data is included in the package
@@ -39,36 +37,7 @@
 #' ## The data is compact enough to be loaded in memory
 #' print(object.size(data), units="Kb")
 
-loadCoverage <- function(chr, datadir=NULL, sampledirs=NULL, samplepatt=NULL, cutoff=5, chrlen=NULL, bamterm="accepted_hits.bam", output=NULL, verbose=TRUE) {
-	## Determine the full paths to the sample directories
-	if(!is.null(sampledirs)) {
-		if(!is.null(datadir)) {
-			## Using sampledirs with datadir
-			dirs <- sapply(sampledirs, function(x) { file.path(datadir, x) })
-			names(dirs) <- sampledirs
-		} else {
-			## Using only the sampledirs since datadir is NULL
-			dirs <- sampledirs
-			names(dirs) <- sampledirs
-		}
-	} else if (!is.null(samplepatt)) {
-		if(is.null(datadir)) {
-			## This case assumes that the datadir is the current directory
-			datadir <- "."
-		}
-		## Identify the directories with this pattern
-		dirs <- dir(path=datadir, pattern=samplepatt, full.names=TRUE)
-		names(dirs) <- dir(path=datadir, pattern=samplepatt, full.names=FALSE)
-	} else {
-		stop("Either 'samplepatt' or 'sampledirs' must be non-NULL.")
-	}
-	
-	## Tell R which are the BAM files
-	if(!is.null(bamterm)) {
-		tmp <- file.path(dirs, bamterm)
-		names(tmp) <- names(dirs)
-		dirs <- tmp
-	}
+loadCoverage <- function(dirs, chr, cutoff=5, chrlen=NULL, output=NULL, verbose=TRUE) {
 	## Do the indexes exist?
 	bai <- paste0(dirs, ".bai")
 	if(all(file.exists(bai))) {
