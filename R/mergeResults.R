ec6230029b2471369a9ae745ee9f0e3fb8eb1152
lcolladotor <lcollado@jhsph.edu>
2013-09-17 18:21:36 -0400

    Debugging

--- R/mergeResults.R
+++ R/mergeResults.R
@@ -24,7 +24,7 @@
 #' @seealso \link{analyzeChr}, \link{calculatePvalues}, \link{annotateRegions}
 #' @export
 #' @importFrom GenomicRanges GRangesList
-#' @importMethodsFrom GenomicRanges unlist "$" "$<-" "[" elementMetadata "elementMetadata<-"
+#' @importMethodsFrom GenomicRanges unlist "$" "$<-" "["
 #' @importFrom IRanges DataFrame RleList
 #' @importMethodsFrom IRanges cbind values "values<-" "[" "$" "$<-" length order unlist as.numeric nrow
 #' @importFrom qvalue qvalue
@@ -137,18 +137,26 @@ mergeResults <- function(chrnums=c(1:22, "X", "Y"), prefix=".", significantCut=c
 	if(verbose) message(paste(Sys.time(), "mergeResults: Saving fullNullSummary"))
 	save(fullNullSummary, file=file.path(prefix, "fullNullSummary.Rdata"))
 		
+	print(1)
 	if(nrow(fullNullSummary) > 0) {
 		## Actual calculation
+		print(2)
 		nullareas <- as.numeric(fullNullSummary$area)
+		print(3)
 		pvals <- sapply(fullRegions$area, function(x) { sum(nullareas > x) })
 	
 		## Update info
+		print(4)
 		fullRegions$pvalues <- (pvals + 1) / (length(nullareas) + 1)
+		print(5)
 		fullRegions$qvalues <- qvalue(fullRegions$pvalues)$qvalues
+		print(6)
 		fullRegions$significant <- factor(fullRegions$pvalues < significantCut[1], levels=c(TRUE, FALSE))
+		print(7)
 		fullRegions$significantQval <- factor(fullRegions$qvalues < significantCut[2], levels=c(TRUE, FALSE))
 	}	
 	## Sort by decreasing area
+	print(8)
 	fullRegions <- fullRegions[order(fullRegions$area, decreasing=TRUE)]
 	
 	## save GRanges version
