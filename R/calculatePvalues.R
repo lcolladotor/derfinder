cffd844ddc0dba2c32a8eac1a8f10e674318c96e
lcolladotor <lcollado@jhsph.edu>
2013-09-25 18:30:40 -0400

    Fixed the column ordering when calculatePvalues() finds no null regions

--- R/calculatePvalues.R
+++ R/calculatePvalues.R
@@ -216,8 +216,8 @@ calculatePvalues <- function(coveragePrep, models, fstats, nPermute = 1L, seeds 
 	} else {
 		if(verbose) message(paste(Sys.time(), "calculatePvalues: no null regions found. Skipping p-value calculation."))
 		regs$pvalues <- rep(NA, length(regs))
-		regs$qvalues <- rep(NA, length(regs))
 		regs$significant <- rep(NA, length(regs))
+		regs$qvalues <- rep(NA, length(regs))
 		regs$significantQval <- rep(NA, length(regs))
 	}
 	## Save the nullstats too
