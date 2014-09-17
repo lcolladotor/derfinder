dff629188a4b315831299665df707144ebec7481
lcolladotor <lcollado@jhsph.edu>
2014-09-16 20:10:19 -0400

    Rename RUN_ALL_TESTS to R_CHECK_TESTS for consistency with other environment variables used in r-travis

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,11 +1,11 @@
-## Run the tests if the system variable 'RUN_ALL_TESTS' is set to TRUE
+## Run the tests if the system variable 'R_CHECK_TESTS' is set to TRUE
 
-flag <- as.logical(Sys.getenv('RUN_ALL_TESTS'))
+flag <- as.logical(Sys.getenv('R_CHECK_TESTS'))
 if(!is.na(flag)) {
     if(flag == TRUE) {
         library('testthat')
         test_check('derfinder')
     }
 } else {
-    message('Set the system variable RUN_ALL_TESTS to TRUE to run all the tests.')
+    message('Set the system variable R_CHECK_TESTS to TRUE to run all the tests.')
 }
