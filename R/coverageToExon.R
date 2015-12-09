lcolladotor/derfinder

commit cf87480ab22bd6bcdb3208821b123f2720e114a8

v1.5.11 -- Now 'L' can be a vector of length equal to the number of samples, and railMatrix() has become more versatile

DESCRIPTION
@@ -1,9 +1,9 @@
 Package: derfinder
 Type: Package
-Title: Annotation-agnostic differential expression analysis of RNA-seq data at 
+Title: Annotation-agnostic differential expression analysis of RNA-seq data at
     base-pair resolution via the DER Finder approach
-Version: 1.5.10
-Date: 2015-11-30
+Version: 1.5.11
+Date: 2015-12-09
 Authors@R: c(person("Leonardo", "Collado-Torres", role = c("aut", "cre"), email
     = "lcollado@jhu.edu"), person(c("Alyssa", "C."), "Frazee", role = c("aut"),
     email = "alyssa.frazee@gmail.com"), person(c("Andrew", "E."), "Jaffe",
@@ -42,12 +42,13 @@ Suggests:
     testthat,
     TxDb.Hsapiens.UCSC.hg19.knownGene
 VignetteBuilder: knitr
-Description: This package provides functions for annotation-agnostic 
+Description: This package provides functions for annotation-agnostic
     differential expression analysis of RNA-seq data. Two implementations of
-    the DER Finder approach are included in this package: (1) single-base level 
+    the DER Finder approach are included in this package: (1) single-base level
     F-statistics and (2) identification of expressed-regions.
 License: Artistic-2.0
 LazyData: true
 URL: https://github.com/lcolladotor/derfinder
 BugReports: https://github.com/lcolladotor/derfinder/issues
 biocViews: DifferentialExpression, Sequencing, RNASeq, Software
+RoxygenNote: 5.0.1

NAMESPACE
@@ -1,4 +1,4 @@
-# Generated by roxygen2 (4.1.1): do not edit by hand
+# Generated by roxygen2: do not edit by hand
 
 export(advancedArg)
 export(advanced_arg)
@@ -112,6 +112,7 @@ importMethodsFrom(GenomicRanges,'[')
 importMethodsFrom(GenomicRanges,'[<-')
 importMethodsFrom(GenomicRanges,'names<-')
 importMethodsFrom(GenomicRanges,'strand<-')
+importMethodsFrom(GenomicRanges,'values<-')
 importMethodsFrom(GenomicRanges,as.data.frame)
 importMethodsFrom(GenomicRanges,c)
 importMethodsFrom(GenomicRanges,countOverlaps)
@@ -125,7 +126,10 @@ importMethodsFrom(GenomicRanges,names)
 importMethodsFrom(GenomicRanges,reduce)
 importMethodsFrom(GenomicRanges,resize)
 importMethodsFrom(GenomicRanges,sort)
+importMethodsFrom(GenomicRanges,split)
 importMethodsFrom(GenomicRanges,strand)
+importMethodsFrom(GenomicRanges,subset)
+importMethodsFrom(GenomicRanges,values)
 importMethodsFrom(GenomicRanges,width)
 importMethodsFrom(IRanges,'$<-')
 importMethodsFrom(IRanges,'[')

R/coverageToExon.R
@@ -7,7 +7,8 @@
 #' 
 #' @inheritParams getRegionCoverage
 #' @inheritParams annotateRegions
-#' @param L The width of the reads used.
+#' @param L The width of the reads used. Either a vector of length 1 or length
+#' equal to the number of samples.
 #' @param returnType If \code{raw}, then the raw coverage information per exon 
 #' is returned. If \code{rpkm}, RPKM values are calculated for each exon.
 #' @inheritParams fullCoverage
@@ -176,7 +177,15 @@ coverageToExon <- function(fullCov = NULL, genomicState, L = NULL,
         g <- e[seqnames(e) == chr]
         ind <- rep(names(g), width(g))  # to split
         tmpList <- split(z, ind)  # split
-        res <- t(sapply(tmpList, colSums)/L)  # get # reads
+        res <- t(sapply(tmpList, colSums))  # get # reads
+        
+        if(length(L) == 1) {
+            res <- res / L
+        } else if (length(L) == ncol(res)) {
+            res <- res / matrix(rep(L, each = nrow(res)), ncol = ncol(res))
+        } else {
+            warning("Invalid 'L' value so it won't be used. It has to either be a integer/numeric vector of length 1 or length equal to the number of samples.")
+        }
     
         # done
         return(res)

R/railMatrix.R
@@ -31,9 +31,11 @@
 #' only when \code{totalMapped} is specified. By default, it adjusts to 
 #' libraries with 40 million reads, which matches the default used in Rail.
 #' @inheritParams filterData
-#' @param L The width of the reads used.
+#' @param L The width of the reads used. Either a vector of length 1 or length
+#' equal to the number of samples.
 #' @param maxClusterGap This determines the maximum gap between candidate ERs.
 #' @param returnBP If \code{TRUE}, returns \code{$bpCoverage} explained below.
+#' @param file.cores Number of cores used for loading the BigWig files per chr.
 #' @param ... Arguments passed to other methods and/or advanced arguments.
 #'
 #' @return A list with one entry per chromosome. Then per chromosome, a list 
@@ -90,7 +92,7 @@
 #' }
 
 
-railMatrix <- function(chrs, summaryFiles, sampleFiles, L = NULL, cutoff = NULL, maxClusterGap = 300L, totalMapped = NULL, targetSize = 40e6, returnBP = TRUE, ...) {
+railMatrix <- function(chrs, summaryFiles, sampleFiles, L = NULL, cutoff = NULL, maxClusterGap = 300L, totalMapped = NULL, targetSize = 40e6, returnBP = TRUE, file.cores = 1L, ...) {
     stopifnot(length(chrs) == length(summaryFiles))
     ## In the future, summaryFiles might be a vector or a list, but the length
     ## check must be maintained. It might be a list if the mean bigwig info
@@ -101,13 +103,14 @@ railMatrix <- function(chrs, summaryFiles, sampleFiles, L = NULL, cutoff = NULL,
     
     ## Define cluster
     BPPARAM <- .define_cluster(...)
+    BPPARAM.railChr <- .define_cluster(cores = 'file.cores', file.cores = file.cores, ...)
     
     regionMat <- bpmapply(.railMatrixChr, chrs, summaryFiles, SIMPLIFY = FALSE, MoreArgs = list(sampleFiles = sampleFiles, L = L, maxClusterGap = maxClusterGap, cutoff = cutoff, totalMapped = totalMapped, targetSize = targetSize, returnBP = returnBP), BPPARAM = BPPARAM)
     return(regionMat)
 }
 
 
-.railMatrixChr <- function(chr, summaryFile, sampleFiles, L = NULL, cutoff = NULL,  maxClusterGap = 300L, totalMapped = NULL, targetSize = 40e6, returnBP = TRUE) {
+.railMatrixChr <- function(chr, summaryFile, sampleFiles, L = NULL, cutoff = NULL,  maxClusterGap = 300L, totalMapped = NULL, targetSize = 40e6, returnBP = TRUE, BPPARAM.railChr = BPPARAM.railChr) {
     meanCov <- loadCoverage(files = summaryFile, chr = chr)
     regs <- findRegions(position = Rle(TRUE, length(meanCov$coverage[[1]])), fstats = meanCov$coverage[[1]], chr = chr, maxClusterGap = maxClusterGap, cutoff = cutoff)
     
@@ -118,9 +121,21 @@ railMatrix <- function(chrs, summaryFiles, sampleFiles, L = NULL, cutoff = NULL,
     seqlengths(regs) <- length(meanCov$coverage[[1]])
     
     ## Get the region coverage matrix
-    regionCov <- getRegionCoverage(regions = regs, files = sampleFiles, which = regs, totalMapped = totalMapped, targetSize = targetSize, mc.cores = 1L, protectWhich = 0)
+    fullCov <- fullCoverage(files = sampleFiles, chrs = chr, protectWhich = 0,
+        which = regs, totalMapped = totalMapped, targetSize = targetSize,
+        BPPARAM.custom = BPPARAM.railChr)
+    regionCov <- getRegionCoverage(fullCov = fullCov, regions = regs,
+        mc.cores = 1L)
     covMat <- lapply(regionCov, colSums)
-    covMat <- do.call(rbind, covMat) / L
+    covMat <- do.call(rbind, covMat)
+    if(length(L) == 1) {
+        covMat <- covMat / L
+    } else if (length(L) == ncol(covMat)) {
+        covMat <- covMat / matrix(rep(L, each = nrow(covMat)), ncol = ncol(covMat))
+    } else {
+        warning("Invalid 'L' value so it won't be used. It has to either be a integer/numeric vector of length 1 or length equal to the number of samples.")
+    }
+    
     
     ## Finish
     if(returnBP) {

R/regionMatrix.R
@@ -12,7 +12,8 @@
 #' using \link{fullCoverage}. If \code{runFilter = FALSE}, then 
 #' \code{returnMean = TRUE} must have been used.
 #' @inheritParams filterData
-#' @param L The width of the reads used.
+#' @param L The width of the reads used. Either a vector of length 1 or length
+#' equal to the number of samples.
 #' @param runFilter This controls whether to run \link{filterData} or not. If 
 #' set to \code{FALSE} then \code{returnMean = TRUE} must have been used to 
 #' create each element of \code{fullCov}.
@@ -148,7 +149,14 @@ regionMatrix <- function(fullCov, cutoff = 5, filter = 'mean', L,
         chrsStyle = chrsStyle, mc.cores = 1L)
         
     covMat <- lapply(regionCov, colSums)
-    covMat <- do.call(rbind, covMat) / L
+    covMat <- do.call(rbind, covMat)
+    if(length(L) == 1) {
+        covMat <- covMat / L
+    } else if (length(L) == ncol(covMat)) {
+        covMat <- covMat / matrix(rep(L, each = nrow(covMat)), ncol = ncol(covMat))
+    } else {
+        warning("Invalid 'L' value so it won't be used. It has to either be a integer/numeric vector of length 1 or length equal to the number of samples.")
+    }
 
     ## Finish
     if(returnBP) {

man/advancedArg.Rd
@@ -1,4 +1,4 @@
-% Generated by roxygen2 (4.1.1): do not edit by hand
+% Generated by roxygen2: do not edit by hand
 % Please edit documentation in R/advancedArg.R
 \name{advancedArg}
 \alias{advancedArg}
@@ -19,12 +19,12 @@ advancedArg(fun, package = "derfinder", browse = interactive())
 A vector of URLs with the GitHub search queries.
 }
 \description{
-Find in GitHub the documentation for the advanced arguments of a given
+Find in GitHub the documentation for the advanced arguments of a given 
 function.
 }
 \details{
 If you are interested on the default options used for functions
-that run on multiple cores, check
+that run on multiple cores, check 
 https://github.com/lcolladotor/derfinder/blob/master/R/utils.R
 Note that in general, \link[BiocParallel]{SnowParam} is more memory efficient
 than link[BiocParallel]{MulticoreParam}. If you so desire, use your favorite

man/analyzeChr.Rd
@@ -1,4 +1,4 @@
-% Generated by roxygen2 (4.1.1): do not edit by hand
+% Generated by roxygen2: do not edit by hand
 % Please edit documentation in R/analyzeChr.R
 \name{analyzeChr}
 \alias{analyzeChr}
@@ -14,59 +14,69 @@ analyzeChr(chr, coverageInfo, models, cutoffPre = 5, cutoffFstat = 1e-08,
 \item{chr}{Used for naming the output files when \code{writeOutput=TRUE} and
 the resulting \code{GRanges} object.}
 
-\item{coverageInfo}{A list containing a DataFrame --\code{$coverage}-- with
-the coverage data and a logical Rle --\code{$position}-- with the positions
+\item{coverageInfo}{A list containing a DataFrame --\code{$coverage}-- with 
+the coverage data and a logical Rle --\code{$position}-- with the positions 
 that passed the cutoff. This object is generated using \link{loadCoverage}.}
 
 \item{models}{The output from \link{makeModels}.}
 
-\item{cutoffPre}{This argument is passed to \link{preprocessCoverage}
+\item{cutoffPre}{This argument is passed to \link{preprocessCoverage} 
 (\code{cutoff}).}
 
-\item{cutoffFstat}{This is used to determine the cutoff argument of
-\link{calculatePvalues} and it's behaviour is determined by
+\item{cutoffFstat}{This is used to determine the cutoff argument of 
+\link{calculatePvalues} and it's behaviour is determined by 
 \code{cutoffType}.}
 
-\item{cutoffType}{If set to \code{empirical}, the \code{cutoffFstat}
-(example: 0.99) quantile is used via \link{quantile}. If set to
-\code{theoretical}, the theoretical \code{cutoffFstats} (example: 1e-08) is
-calculated via \link{qf}. If set to \code{manual}, \code{cutoffFstats} is
+\item{cutoffType}{If set to \code{empirical}, the \code{cutoffFstat} 
+(example: 0.99) quantile is used via \link{quantile}. If set to 
+\code{theoretical}, the theoretical \code{cutoffFstats} (example: 1e-08) is 
+calculated via \link{qf}. If set to \code{manual}, \code{cutoffFstats} is 
 passed to \link{calculatePvalues} without any other calculation.}
 
-\item{nPermute}{The number of permutations. Note that for a full chromosome,
-a small amount (10) of permutations is sufficient. If set to 0, no
-permutations are performed and thus no null regions are used, however, the
+\item{nPermute}{The number of permutations. Note that for a full chromosome, 
+a small amount (10) of permutations is sufficient. If set to 0, no 
+permutations are performed and thus no null regions are used, however, the 
 \code{$regions} component is created.}
 
-\item{seeds}{An integer vector of length \code{nPermute} specifying the
+\item{seeds}{An integer vector of length \code{nPermute} specifying the 
 seeds to be used for each permutation. If \code{NULL} no seeds are used.}
 
-\item{groupInfo}{A factor specifying the group membership of each sample
-that can later be used with the plotting functions in the
+\item{groupInfo}{A factor specifying the group membership of each sample 
+that can later be used with the plotting functions in the 
 \code{derfinderPlot} package.}
 
-\item{txdb}{This argument is passed to
-\link[bumphunter]{annotateTranscripts}. If \code{NULL},
-\link[TxDb.Hsapiens.UCSC.hg19.knownGene]{TxDb.Hsapiens.UCSC.hg19.knownGene}
+\item{txdb}{This argument is passed to 
+\link[bumphunter]{annotateTranscripts}. If \code{NULL}, 
+\link[TxDb.Hsapiens.UCSC.hg19.knownGene]{TxDb.Hsapiens.UCSC.hg19.knownGene} 
 is used.}
 
-\item{writeOutput}{If \code{TRUE}, output Rdata files are created at each
-step inside a directory with the chromosome name (example: 'chr21' if
-\code{chrnum='21'}). One Rdata file is created for each component described
+\item{writeOutput}{If \code{TRUE}, output Rdata files are created at each 
+step inside a directory with the chromosome name (example: 'chr21' if 
+\code{chrnum='21'}). One Rdata file is created for each component described 
 in the return section.}
 
-\item{runAnnotation}{If \code{TRUE} \link[bumphunter]{annotateTranscripts}
+\item{runAnnotation}{If \code{TRUE} \link[bumphunter]{annotateTranscripts} 
 and \link[bumphunter]{matchGenes} are run. Otherwise these steps are skipped.}
 
-\item{lowMemDir}{If specified, each chunk is saved into a separate Rdata
-file under \code{lowMemDir} and later loaded in
-\link[derfinderHelper]{fstats.apply} when
-running \link{calculateStats} and \link{calculatePvalues}. Using this option
-helps reduce the memory load as each fork in \link[BiocParallel]{bplapply}
-loads only the data needed for the chunk processing. The downside is a bit
+\item{lowMemDir}{If specified, each chunk is saved into a separate Rdata 
+file under \code{lowMemDir} and later loaded in 
+\link[derfinderHelper]{fstats.apply} when 
+running \link{calculateStats} and \link{calculatePvalues}. Using this option 
+helps reduce the memory load as each fork in \link[BiocParallel]{bplapply} 
+loads only the data needed for the chunk processing. The downside is a bit 
 longer computation time due to input/output.}
 
 \item{...}{Arguments passed to other methods and/or advanced arguments.}
+
+\item{verbose}{If \code{TRUE} basic status updates will be printed along the 
+way.}
+
+\item{scalefac}{This argument is passed to \link{preprocessCoverage}.}
+
+\item{chunksize}{This argument is passed to \link{preprocessCoverage}.}
+
+\item{returnOutput}{If \code{TRUE}, it returns a list with the results from 
+each step. Otherwise, it returns \code{NULL}.}
 }
 \value{
 If \code{returnOutput=TRUE}, a list with six components:
@@ -78,31 +88,31 @@ If \code{returnOutput=TRUE}, a list with six components:
 \item{regions}{ The output from \link{calculatePvalues}.}
 \item{annotation}{ The output from \link[bumphunter]{matchGenes}.}
 }
-These are the same components that are written to Rdata files if
+These are the same components that are written to Rdata files if 
 \code{writeOutput=TRUE}.
 }
 \description{
-This is a major wrapper for running several key functions from this package.
-It is meant to be used after \link{loadCoverage} has been used for a
-specific chromosome. The steps run include \link{makeModels},
-\link{preprocessCoverage}, \link{calculateStats}, \link{calculatePvalues}
+This is a major wrapper for running several key functions from this package. 
+It is meant to be used after \link{loadCoverage} has been used for a 
+specific chromosome. The steps run include \link{makeModels}, 
+\link{preprocessCoverage}, \link{calculateStats}, \link{calculatePvalues} 
 and annotating with \link[bumphunter]{annotateTranscripts} and
 \link[bumphunter]{matchGenes}.
 }
 \details{
 If you are working with data from an organism different from 'Homo sapiens'
-specify so by setting the global 'species' and 'chrsStyle' options. For
+specify so by setting the global 'species' and 'chrsStyle' options. For 
 example:
 \code{options(species = 'arabidopsis_thaliana')}
 \code{options(chrsStyle = 'NCBI')}
 }
 \examples{
 ## Collapse the coverage information
-collapsedFull <- collapseFullCoverage(list(genomeData$coverage),
+collapsedFull <- collapseFullCoverage(list(genomeData$coverage), 
     verbose = TRUE)
 
 ## Calculate library size adjustments
-sampleDepths <- sampleDepth(collapsedFull, probs = c(0.5), nonzero=TRUE,
+sampleDepths <- sampleDepth(collapsedFull, probs = c(0.5), nonzero=TRUE, 
     verbose=TRUE)
 
 ## Build the models
@@ -111,8 +121,8 @@ adjustvars <- data.frame(genomeInfo$gender)
 models <- makeModels(sampleDepths, testvars=groupInfo, adjustvars=adjustvars)
 
 ## Analyze the chromosome
-results <- analyzeChr(chr='21', coverageInfo=genomeData, models=models,
-    cutoffFstat=1, cutoffType='manual', groupInfo=groupInfo, mc.cores=1,
+results <- analyzeChr(chr='21', coverageInfo=genomeData, models=models, 
+    cutoffFstat=1, cutoffType='manual', groupInfo=groupInfo, mc.cores=1, 
     writeOutput=FALSE, returnOutput=TRUE, method='regular',
     runAnnotation = FALSE)
 names(results)
@@ -121,8 +131,8 @@ names(results)
 Leonardo Collado-Torres
 }
 \seealso{
-\link{makeModels}, \link{preprocessCoverage},
-\link{calculateStats}, \link{calculatePvalues},
+\link{makeModels}, \link{preprocessCoverage}, 
+\link{calculateStats}, \link{calculatePvalues}, 
 \link[bumphunter]{annotateTranscripts}, \link[bumphunter]{matchGenes}
 }
 

man/annotateRegions.Rd
@@ -1,4 +1,4 @@
-% Generated by roxygen2 (4.1.1): do not edit by hand
+% Generated by roxygen2: do not edit by hand
 % Please edit documentation in R/annotateRegions.R
 \name{annotateRegions}
 \alias{annotateRegions}
@@ -9,34 +9,37 @@ annotateRegions(regions, genomicState, annotate = TRUE, ...)
 \arguments{
 \item{regions}{The \code{$regions} output from \link{calculatePvalues}.}
 
-\item{genomicState}{A GRanges object created with \link{makeGenomicState}.
-It can be either the \code{genomicState$fullGenome} or
+\item{genomicState}{A GRanges object created with \link{makeGenomicState}. 
+It can be either the \code{genomicState$fullGenome} or 
 \code{genomicState$codingGenome} component.}
 
-\item{annotate}{If \code{TRUE} then the regions are annotated by the genomic
-state. Otherwise, only the overlaps between the regions and the genomic
+\item{annotate}{If \code{TRUE} then the regions are annotated by the genomic 
+state. Otherwise, only the overlaps between the regions and the genomic 
 states are computed.}
 
 \item{...}{Arguments passed to other methods and/or advanced arguments.}
+
+\item{verbose}{If \code{TRUE} basic status updates will be printed along the 
+way.}
 }
 \value{
-A list with elements \code{countTable} and \code{annotationList}
-(only if \code{annotate=TRUE}).
+A list with elements \code{countTable} and \code{annotationList} 
+(only if \code{annotate=TRUE}). 
 \describe{
-\item{countTable }{This is a data.frame with the number of overlaps from the
-regions vs the genomic states with one type per column. For example, if
-\code{fullOrCoding='full'} then the columns are \code{exon},
+\item{countTable }{This is a data.frame with the number of overlaps from the 
+regions vs the genomic states with one type per column. For example, if 
+\code{fullOrCoding='full'} then the columns are \code{exon}, 
 \code{intergenic} and \code{intron}.}
-\item{annotationList }{This is a \code{GRangesList} with the genomic states
-that overlapped with the regions. The
-names of this \code{GRangesList} correspond to the region index in
+\item{annotationList }{This is a \code{GRangesList} with the genomic states 
+that overlapped with the regions. The 
+names of this \code{GRangesList} correspond to the region index in 
 \code{regions}.}
 }
 }
 \description{
-This function takes the regions found in \link{calculatePvalues} and assigns
-them genomic states contructed with \link{makeGenomicState}. The main
-workhorse functions are \link[IRanges]{countOverlaps} and
+This function takes the regions found in \link{calculatePvalues} and assigns 
+them genomic states contructed with \link{makeGenomicState}. The main 
+workhorse functions are \link[IRanges]{countOverlaps} and 
 \link[IRanges]{findOverlaps}.
 }
 \details{
@@ -46,7 +49,7 @@ details.
 }
 \examples{
 ## Annotate regions, first two only
-annotatedRegions <- annotateRegions(regions=genomeRegions$regions[1:2],
+annotatedRegions <- annotateRegions(regions=genomeRegions$regions[1:2], 
     genomicState=genomicState$fullGenome, minoverlap=1)
 annotatedRegions
 }

commit f902a1bf3c3fb4eae173a701dbafb5ebd49022c5

Fix NAMESPACE problems introduced in cf87480ab that were fixed by Herve in 0be3a8c

DESCRIPTION
@@ -2,7 +2,7 @@ Package: derfinder
 Type: Package
 Title: Annotation-agnostic differential expression analysis of RNA-seq data at
     base-pair resolution via the DER Finder approach
-Version: 1.5.11
+Version: 1.5.12
 Date: 2015-12-09
 Authors@R: c(person("Leonardo", "Collado-Torres", role = c("aut", "cre"), email
     = "lcollado@jhu.edu"), person(c("Alyssa", "C."), "Frazee", role = c("aut"),

NAMESPACE
@@ -112,7 +112,6 @@ importMethodsFrom(GenomicRanges,'[')
 importMethodsFrom(GenomicRanges,'[<-')
 importMethodsFrom(GenomicRanges,'names<-')
 importMethodsFrom(GenomicRanges,'strand<-')
-importMethodsFrom(GenomicRanges,'values<-')
 importMethodsFrom(GenomicRanges,as.data.frame)
 importMethodsFrom(GenomicRanges,c)
 importMethodsFrom(GenomicRanges,countOverlaps)
@@ -126,10 +125,7 @@ importMethodsFrom(GenomicRanges,names)
 importMethodsFrom(GenomicRanges,reduce)
 importMethodsFrom(GenomicRanges,resize)
 importMethodsFrom(GenomicRanges,sort)
-importMethodsFrom(GenomicRanges,split)
 importMethodsFrom(GenomicRanges,strand)
-importMethodsFrom(GenomicRanges,subset)
-importMethodsFrom(GenomicRanges,values)
 importMethodsFrom(GenomicRanges,width)
 importMethodsFrom(IRanges,'$<-')
 importMethodsFrom(IRanges,'[')

R/coverageToExon.R
@@ -41,7 +41,7 @@
 #' @importFrom GenomeInfoDb seqlevels renameSeqlevels
 #' mapSeqlevels seqlevelsInUse
 #' @importMethodsFrom GenomicRanges names 'names<-' length '[' coverage sort 
-#' width c strand subset as.data.frame
+#' width c strand as.data.frame
 #' @importMethodsFrom IRanges subset as.data.frame
 #' @import S4Vectors
 #' @importFrom BiocParallel bplapply bpmapply

R/makeGenomicState.R
@@ -30,7 +30,7 @@
 #' @importFrom GenomeInfoDb seqlengths seqlevels renameSeqlevels
 #' @importMethodsFrom AnnotationDbi select
 #' @importMethodsFrom GenomicRanges names 'names<-' reduce '$' 
-#' '$<-' '[' '[<-' values 'values<-' sort disjoin length findOverlaps split 
+#' '$<-' '[' '[<-' sort disjoin length findOverlaps
 #' strand 'strand<-' gaps width
 #' @importMethodsFrom IRanges names unlist relist length lapply '['
 #' @import S4Vectors